7ea2b22035c54adb8d93ae881919e6dba81c943a
xiaodao <lychees67@gmail.com>
2016-11-21 06:04:56 +0800

    ping

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
 		appid: 'gh_d115a5a8f4af',
 		token: 'MAGA',
 		encodingAESKey: 'aXlaBMQG4sGCzXhIBnWdcsHTAYBYMGmfCg1Bk41nF5V'
-	}
+	},
 
 	api: {
 		AppId: '88b1ca60c82e6fea54e6e0a43a2f57e5',
