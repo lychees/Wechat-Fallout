4d65d465d481f9f720b475df73fdcdd881cd2986
xiaodao <lychees67@gmail.com>
2016-11-21 15:34:04 +0800

    fx

--- message/msg_text.js
+++ message/msg_text.js
@@ -2,7 +2,9 @@
 // 接收文本消息
 
 var total = 0;
+var http = require('http');
 const config = require('../config');
+
 
 module.exports = function *(message) {
 	//let {Content: text} = message;
@@ -13,15 +15,16 @@ module.exports = function *(message) {
 
 	//https://mp.weixin.qq.com/wiki/14/bb5031008f1494a59c6f71fa0f319c66.html
 	// https://cnodejs.org/topic/51de0a33f4963ade0e094fce
-	//	https://api.weixin.qq.com/cgi-bin/user/info?access_token=ACCESS_TOKEN&openid=OPENID&lang=zh_CN
+	//	https://api.weixin.qq.com/cgi-bin/user/info?access_token=ACCESS_TOKEN&openid=OPENID&lang=zh_CN  
+
+	// https://github.com/node-webot/wechat
+	// 消息接口指南。
 
 	var API = require('wechat-api');  
 	var api = new API(config.api.AppId, config.api.AppSecret);  
-
 	return api.getAccessToken(function (err, token) {  
     	console.log(err);  
     	console.log(token);  
-
 		http.get("https://api.weixin.qq.com/cgi-bin/user/info?access_token=" + token + "&openid" + message.FromUserName + "=OPENID&lang=zh_CN", function(res) {
   			console.log("Got response: " + res.statusCode);
   			return "欢迎你，" + res.nickname + '。 第' + String(total) + '条消息.';
