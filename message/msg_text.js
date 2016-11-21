afd11ec12ceff5e4f9ae7f46de2a87f350285220
xiaodao <lychees67@gmail.com>
2016-11-21 17:53:55 +0800

    fx

--- message/msg_text.js
+++ message/msg_text.js
@@ -14,14 +14,13 @@ module.exports = function *(message) {
 
 	//https://mp.weixin.qq.com/wiki/14/bb5031008f1494a59c6f71fa0f319c66.html
 	// https://cnodejs.org/topic/51de0a33f4963ade0e094fce
-	//	https://api.weixin.qq.com/cgi-bin/user/info?access_token=ACCESS_TOKEN&openid=OPENID&lang=zh_CN  
 
 	// https://github.com/node-webot/wechat
 	// 消息接口指南。
 
 	var API = require('wechat-api');  
 	var api = new API(config.api.AppId, config.api.AppSecret);  
-	return api.getAccessToken(function (err, token) {  
+/*	return api.getAccessToken(function (err, token) {  
     	console.log(err);  
     	console.log(token);  
 		return http.get("http://api.weixin.qq.com/cgi-bin/user/info?access_token=" + token + "&openid=" + message.FromUserName + "&lang=zh_CN", function(res) {
@@ -32,7 +31,14 @@ module.exports = function *(message) {
   			console.log("Got error: " + e.message);
   			return 'Error, 第' + String(total) + '条消息.';
 		});
-	});  
+	});  */
+
+	api.getUser({openid: message.FromUserName, lang: 'en'}, function (err, result){
+		console.log(result);
+		return result.nickname;
+	});
+	
+
 
 	console.log ('why?');
 	return "why?";
